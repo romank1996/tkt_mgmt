2a7850a53fd9278178cb6656dfceda27f78f3a79
Jenish Acharya <jenish.acharya.c5@gmail.com>
2020-09-13 11:09:15 +0545

    accounts updated to accomodate addition of contact_no in signup form

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('updateprofile/<int:pk>', views.update_profile, name ='updateprofile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login, authenticate
 from  django.contrib.auth.forms import UserCreationForm
-from .forms import ProfileForm
+
+from .forms import ProfileForm, SignUpForm
 from . models import Profile
 
 # Create your views here.
 
+# class based view for signup form
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+# function based view used to accomodate contact_no in sign up form
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.contact_no = form.cleaned_data.get('contact_no')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 def update_profile(request, pk):
     template = 'profile_update.html'
