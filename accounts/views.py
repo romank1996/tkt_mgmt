9c2c65450c20c4fdfe0ff62bc704a3301668b4b1
Jenish Acharya <jenish.acharya.c5@gmail.com>
2020-10-13 23:16:40 +0545

    user activation from email on user registration , added

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login, authenticate, update_session_auth_hash
 from  django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
 
 from django.contrib.auth.models import Group
 from .forms import ProfileForm, SignUpForm
@@ -27,6 +35,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()
+            user.is_active = False
             user.profile.first_name = form.cleaned_data.get('first_name')
             user.profile.last_name = form.cleaned_data.get('last_name')
             user.profile.email = form.cleaned_data.get('email')
@@ -34,6 +43,21 @@ def signup(request):
             user.save()
             group = Group.objects.get(name='user')
             user.groups.add(group)
+
+            current_site = get_current_site(request)
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+
+            mail_subject = "Activate your account."
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+            
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
@@ -41,6 +65,23 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 # function based view used to accomodate contact_no in sign up form
 @allowed_user(allowed_roles=['admin'])
