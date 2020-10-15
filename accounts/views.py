5fab4ea74d22b17ebd636b6f4b01f1899e38a8cd
Roman Khwaunju <31197041+romank1996@users.noreply.github.com>
2020-10-15 11:05:44 +1100

    not auto loggin in after email confirmation

--- accounts/views.py
+++ accounts/views.py
@@ -72,7 +72,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        # login(request, user)
         return render(request, 'activation_confirmed.html')
     else:
         return render(request, 'invalid_link.html')
